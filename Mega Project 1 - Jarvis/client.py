a664e61a038ab3b35824e102d7157e5b80f06c71
praneethkotte <praneeth.kotte9@gmail.com>
2025-11-25 20:16:03 +0530

    new changes with environment

--- Mega Project 1 - Jarvis/client.py
+++ Mega Project 1 - Jarvis/client.py
@@ -1,17 +1,20 @@
 from openai import OpenAI
- 
-# pip install openai 
+
+# pip install openai
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key="<Your Key Here>",
+    api_key="pub_efd2a7c74921469785539905553be0c4",
 )
 
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
-    {"role": "user", "content": "what is coding"}
-  ]
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud",
+        },
+        {"role": "user", "content": "what is coding"},
+    ],
 )
 
 print(completion.choices[0].message.content)
